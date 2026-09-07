cd2bd7437d20656288edf7e1c408847c6541bd35
LukaLondaridzen <tactusglacies@gmail.com>
2026-09-07 20:49:10 +0400

    added main.py file

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+import aiohttp
+
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            return await response.json()
+
+
+async def main():
+    urls = [f"https://jsonplaceholder.typicode.com/posts/{i}" for i in range(1, 101)]
+    conc = await asyncio.gather(
+        *[fetch(url) for url in urls]
+    )
+    print(conc)
+
+
+asyncio.run(main())
